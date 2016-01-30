0bf332ed524314b35d24e408c37ecd2c0946c576
liavt <liavt@yahoo.com>
2016-01-30 14:15:10 -0600

    Made enum

--- src/org/usfirst/frc/team5431/robot/Robot.java
+++ src/org/usfirst/frc/team5431/robot/Robot.java
@@ -14,20 +14,21 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
  * directory.
  */
 public class Robot extends IterativeRobot {
-    final String defaultAuto = "Default";
-    final String customAuto = "My Auto";
-    String autoSelected;
-    SendableChooser chooser;
+	enum AutoTask{
+		Default,Custom
+	};
+    AutoTask currentAuto;
+    //SendableChooser chooser;
 	
     /**
      * This function is run when the robot is first started up and should be
      * used for any initialization code.
      */
     public void robotInit() {
-        chooser = new SendableChooser();
-        chooser.addDefault("Default Auto", defaultAuto);
-        chooser.addObject("My Auto", customAuto);
-        SmartDashboard.putData("Auto choices", chooser);
+        // chooser = new SendableChooser();
+        // chooser.addDefault("Default Auto", defaultAuto);
+        // chooser.addObject("My Auto", customAuto);
+        // SmartDashboard.putData("Auto choices", chooser);
     }
     
 	/**
@@ -40,20 +41,20 @@ public class Robot extends IterativeRobot {
 	 * If using the SendableChooser make sure to add them to the chooser code above as well.
 	 */
     public void autonomousInit() {
-    	autoSelected = (String) chooser.getSelected();
-//		autoSelected = SmartDashboard.getString("Auto Selector", defaultAuto);
-		System.out.println("Auto selected: " + autoSelected);
+//     	autoSelected = (String) chooser.getSelected();
+// //		autoSelected = SmartDashboard.getString("Auto Selector", defaultAuto);
+// 		System.out.println("Auto selected: " + autoSelected);
     }
 
     /**
      * This function is called periodically during autonomous. Effect changes based on the value of {@link #autoSelected}.
      */
     public void autonomousPeriodic() {
-    	switch(autoSelected) {
-    	case customAuto:
+    	switch(currentAuto) {
+    	case Custom:
         //Put custom auto code here   
             break;
-    	case defaultAuto:
+    	case Default:
     	default:
     	//Put default auto code here
             break;
