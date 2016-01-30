47087cfa8e591ec9e8209220d33ea9117365a15e
liavt <liavt@yahoo.com>
2016-01-30 14:53:20 -0600

    Docs

--- src/org/usfirst/frc/team5431/map/MotorMap.java
+++ src/org/usfirst/frc/team5431/map/MotorMap.java
@@ -1,8 +1,23 @@
 package org.usfirst.frc.team5431.map;
 
-public class MotorMap {
-
+/**
+ * Class that defines the ID's for the motors.
+ * 
+ * @see CANTalon
+ **/
+public final class MotorMap {
+	//to prevent instantiation
+	private MotorMap(){}
+	
+	/**
+	 * Channel ID for the left flywheel in the shooter mechanism, which is {@value #LeftFly}
+	 * @see Robot
+	 **/
 	public static final int LeftFly = 4;
+		/**
+	 * Channel ID for the right flywheel in the shooter mechanism, which is {@value #RightFly}
+	 * @see Robot
+	 **/
 	public static final int RightFly = 5;
 	
 	
