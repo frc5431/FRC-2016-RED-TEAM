5c29b49516020b8ca7d88af92246ac71491e1064
liavt <liavt@yahoo.com>
2016-02-02 20:06:50 -0600

    wow. this is insane?!?!? documentation

--- src/org/usfirst/frc/team5431/libs/DriveBase.java
+++ src/org/usfirst/frc/team5431/libs/DriveBase.java
@@ -6,18 +6,30 @@ import org.usfirst.frc.team5431.map.MotorMap;
 import edu.wpi.first.wpilibj.CANTalon;
 import edu.wpi.first.wpilibj.RobotDrive;
 
-
+/**
+ * Class that handles tank drive.
+ * 
+ * @author Team 5431
+ * */
 public class DriveBase {
 	
 	private CANTalon rearleft, frontleft, rearright, frontright;
 	
 	private RobotDrive drive;
-	
+	/**
+	 * Default constructor
+	 * 
+	 * @see #DriveBase(boolean b)
+	 * */
 	public DriveBase()
 	{
 		this(false);
 	}
-	
+	/**
+	 * Construtor with an option to have brakemode enabled
+	 * 
+	 * @param brakeMode Whether to enable brakemode on the {@linkplain CANTalon motors}
+	 * */
 	public DriveBase(boolean brakeMode)
 	{
 		this.rearleft = new CANTalon(MotorMap.RearLeft);
@@ -43,11 +55,21 @@ public class DriveBase {
 		this.drive = new RobotDrive(this.frontleft, this.rearleft, this.frontright, this.rearright);
 	}
 	
+	/**
+	 * Uses {@linkplain RobotDrive#tankDrive(double l, double r) tank drive} to drive.
+	 * 
+	 * @param left Value of the left joystick, where -1 is the lowest, 0 is the center, and 1 is the highest.
+	 * @param right Value of the right joystick, where -1 is the lowest, 0 is the center, and 1 is the highest.
+	 * */
 	public void drive(double left, double right)
 	{
 		drive.tankDrive(left, right);
 	}
 	
+	/**
+	 * Checks input and drives based on an {@linkplain OI OI}
+	 * @param map Current operator interface.
+	 * */
 	public void checkInput(OI map){
 		this.drive(map.getDriveLeftYAxis(),map.getDriveRightYAxis());
 	}
